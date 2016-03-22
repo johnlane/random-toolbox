0103eff104ca1bd90432a9b9831f1a7dfd601202
John Lane <johnlane.github@jelmail.com>
2016-03-22 11:45:41 +0000

    RSA cryptosystem example implementation

---
+++ examples/crypto/rsa/rsa.rb
@@ -0,0 +1,135 @@
+#!/usr/bin/ruby
+#
+# Understanding the maths behind RSA
+#
+# This work by John lane 20160316
+
+# Simple assertion takes a list of booleans
+# It raises an exception if any are false or if the list is empty
+def assert(*args)
+  assert false unless args.length > 0
+  args.each { |a| raise RuntimeError, "Assertion Failed", caller unless a }
+end
+
+# Return a random number
+# It ensures that the RNG returns a number (it retries if RNG returns nil).
+# (it will get stuck if the RNG only returns nil)
+# It defaults to the Kernel object's RNG, but optionally define constant RG
+# to any object that responds to #rand and accepts an integer or range
+# (see Kernel#rand for expected behaviour). 
+# Suggestions:
+RG = Random
+# RG = Random.new
+def random(*args)
+  r = (defined?(RG) ? RG : Kernel).rand(*args) while r.nil?
+  r
+end
+
+# Return greatest common divisor of a and b using Euclid's Algorithm.
+# Typically the arguments are given such that a>b but it also works
+# when a<b - an extra iteration will occur which swaps them around.
+def gcd(a,b)
+  (r = a % b) == 0 ? b : gcd(b,r)
+end
+
+# Return coefficients s and t for the relationship gcd(a,b) = sa+tb
+# using the Extended Euclidean Algorithm (sa+tb is called Bézout's identity)
+def eea(a,b)
+  return b==0 ? [1,b] : begin
+    q, r = a.divmod b
+    s, t = eea(b,r)
+    [t, s - q * t]
+  end 
+end
+
+# Return true if a and b are coprime (relatively prime), false otherwise
+# two numbers are coprime if their greatest common divisor is 1
+def coprime(a,b)
+  gcd(a,b) == 1
+end
+
+# Euler's Totient Function - phi(n) 
+# the number of nonzero integers less than n that are relatively prime to n
+# if given one argument then the iterative calculation is performed
+# if given two arguments then assumes they're prime and coprime and calculates directly
+def phi(*args)
+  print args.length; $stdout.flush
+  case args.length
+  when 2
+    # phi = (p-1)(q-1) is valid only for coprime p and q
+    assert coprime(*args)
+    (args[0]-1)*(args[1]-1)
+  when 1
+    n = args[0]
+    (1..n).reduce(0) { |p,i| gcd(n,i)==1 ? p+1 : p }
+  else
+    nil
+  end
+end
+
+# Return a prime number less than n
+require 'prime'
+def prime(n)
+  p = 0
+  loop do
+    p = random(n)
+    break if p.prime?
+  end
+  p
+end
+
+# RSA private key generator
+# Returns private key for public key e and modulus
+# the modlus can be given as a number n
+# or as a pair of primes [p,q].
+# It is only practical to use [p,q] when n is large - this is what secures RSA.
+def key(e, *args)
+  case args.length
+  when 1
+    n = args[0]
+    phi = phi(n) # this is the "hard" thing when n is big!
+  when 2
+    # p and q must be distinct primes and coprime with e
+    args.each { |i| assert i.prime?, coprime(e,i-1) }
+    p, q = *args
+    assert p != q
+
+    n = p*q
+    phi = phi(p,q)
+  else
+    raise ArgumentError
+  end
+
+  # modulus n must be n>2 otherwise, for n=2 you get phi(2)=1
+  # which is effectively modulo 1. Modulo 1 is zero for any n
+  # and can therefore not satisfy Euler's Theorem.
+  assert n>2
+
+  # From Euler's theorem, e and d must be coprime so that:
+  # e.d = phi(n)+1 (mod phi(n)) => e.d +k.phi(n) = 1 (Bézout's Identity)
+  assert coprime(e,phi)
+
+  # Because e is coprime to phi(n), the multiplicative inverse of e with
+  # respect to phi(n) can be efficiently and quickly determined using the
+  # Extended Euclidean Algorithm.  This multiplicative inverse is the private key.
+  a,b = eea(e,phi)               # Extended Euclidean Algorithm (we don't need b
+  assert (a*e)%phi == gcd(e,phi) # Bézout's Identity             except to assert!)
+
+  # The private key is the coefficient a (mod phi(n)). We apply mod phi(n) in
+  # case a is negative; this returns a positive congruence
+  d = a % phi                           # Private key
+  assert d*e%phi == 1                   # Euler's theorem
+  puts "a:#{a}\t\tb:#{b}\t\td:#{d}\te:#{e}\tphi:#{phi}" if LOG
+
+  [d, n]
+
+end
+
+# RSA cipher function
+def cipher(m, e, n)
+  assert m < n
+  assert coprime(g=gcd(m,n),n/g)
+  #above assertion replaces below as per http://crypto.stackexchange.com/questions/33802
+  #assert coprime(m,n) # for Euler's theorem so m^phi(n) = 1 (mod phi(n))
+  m**e % n
+end
---
+++ examples/crypto/rsa/testrsa.rb
@@ -0,0 +1,81 @@
+#!/usr/bin/ruby
+#
+# Understanding the maths behind RSA
+# 
+# Test program
+#
+# This work by John lane 20160316
+
+require_relative 'rsa'
+require 'primes/utils'
+
+# RSA tester
+# Exercises #key and #cipher
+def rsa(m, e, *args)
+  puts "m:#{m}\te:#{e}\targs:#{args.join(',')}" if LOG
+  d, n = key(e,*args)
+  assert (mm = cipher(c = cipher(m,e,n),d,n)) == m
+  puts "n:#{n}\td:#{d}\tc:#{c}\tmm:#{mm}\tphi:#{phi(*args)}\t" if LOG
+end
+
+# Key length in bits, which is the size of the modulus n
+# Above 16 bits the delay incurred by computing phi is apparent
+# By 22 bits the delay is very obvious. This is what makes cracking
+# RSA a "hard" problem. You don't want to do it for 2048 bits!
+BITS = 20
+LOG=true
+
+1000000.times do
+  n=m=g=nil
+
+  # Default private exponent (usually, but not necessarily, prime)
+  # It's usual, although not necessary, to choose 65537 which is
+  # a Fermat prime (see https://en.wikipedia.org/wiki/Fermat_prime).
+  # It is conjectured that there are only five Fermat Primes
+  # and they are 3, 5, 17, 257, and 65537.
+  e=[3,5,17,257,65537].sample
+
+  # randomly execute with a random n or random primes [p,q]
+  case random(1..2)
+  when 1
+    # choose any value for n but cannot go below 3 because
+    # phi(2) is 1 (see assertion in code)
+    n = random(3..1<<BITS-1)
+    args = [n]
+  when 2
+    # Choose two similar-sized but different prime numbers of
+    # half the desired key length. They must be different so we
+    # can efficiently compute Phi (using phi = (p-1)(q-1) which
+    # is valid only for coprime p and q)
+    p = q = prime(BITS/2)
+    until p != q && coprime(p,q) do
+      q = prime(BITS/2)
+    end
+    n = p*q # just for below, not passed to algorithm
+    args = [p,q]
+  end
+
+  phi_n = phi(*args) # just for below, not passed to algorithm
+
+  # choose another public exponent if the requested one is
+  # unsuitable: it must be coprime with phi(n). It could be
+  # bigger then phi(n) but there is no point because e is
+  # congruent mod phi(n) anyway.
+  #until !e.nil? && coprime(e,phi(*args))
+  until coprime(e,phi_n)
+    e = random(1..phi_n-1)
+  end
+  args.unshift(e)
+
+  # choose m - gcd(n,m) must be coprime with n/gcd(n,m)
+  puts "run with m:#{m}\tn:#{n}" if LOG
+  until !m.nil? && coprime(g,n/g) #coprime(n,m)
+    m = random(1..n-1)
+    g = gcd(n,m)
+    puts "m:#{m}\tg:#{g}\tn:#{n}" if LOG
+  end
+  args.unshift(m)
+
+  rsa(*args)
+
+end
